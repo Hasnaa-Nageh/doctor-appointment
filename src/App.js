50af621e5eaca5e5ead9447ecf0f70a30eaa3599
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-10-05 18:30:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./index.css";
@@ -12,7 +13,27 @@ import DoctorDetails from "./pages/DoctorDetails";
 import MyAppointments from "./pages/MyAppointments";
 import Service from "./pages/Service";
 import About from "./pages/About";
+import Logo from "./imgs/Logo0.png";
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading-screen">
+        <img src={Logo} alt="Loading..." className="loading-logo" />
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
   return (
     <>
       <Navbar />
